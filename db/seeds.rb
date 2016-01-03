1a5a88ab70867fcf6a2b27a9875c053e81b4c132
Brian Lu <brianlu365@gmail.com>
2016-01-03 10:16:00 -0800

    fix seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!({name: 'brian'})
 User.create!({name: 'grant'})
-RecycleItem.create!({barcode: "611269991000", points: 2, weight: 1.0, name: "red bull regular"})
-RecycleItem.create!({barcode: "611269142631", points: 2, weight: 1.0, name: "red bull zero - orange"})
+RecyclableItem.create!({barcode: "611269991000", points: 2, weight: 1.0, name: "red bull regular"})
+RecyclableItem.create!({barcode: "611269142631", points: 2, weight: 1.0, name: "red bull zero - orange"})
 
