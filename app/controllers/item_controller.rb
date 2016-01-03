098d62bfd9fe1d0353b35409d2ca53daf0b70f70
Brian Lu <brianlu365@gmail.com>
2016-01-03 00:40:17 -0800

    add ifttt callback handle.

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,4 +1,11 @@
 class ItemController < ApplicationController
+  protect_from_forgery with: :null_session
+  skip_before_action  :verify_authenticity_token
+  #I get something like this
+  #{type:entity,id:5bc053ccfb1646ceaa455fcd4a52f846,attributes:{title:611269357011,tags:[],images:[]},links:{creative:/1/creatives?entity=5bc053ccfb1646ceaa455fcd4a52f846}}
   def collect
+    barcode = params[:attributes][:title]
+    @item = RecyclableItem.where(barcode: barcode).first
+    render json: {status: :ok, item: @item}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'page#home'
-  post 'item/collect'
+  post 'item/collect', to: 'item#collect'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
